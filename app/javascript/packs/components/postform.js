ec0961a0317dd569fcb157995074679abee274dd
jwmunn <contact@justinmunn.co>
2018-01-18 04:11:36 -0800

    Capture and log form data to the console on submit.

--- app/javascript/packs/components/postform.js
+++ app/javascript/packs/components/postform.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 
 class PostForm extends React.Component {
+  onSubmit(e) {
+    e.preventDefault();
+    const formData = {
+      title: this.title.value,
+      body: this.body.value
+    };
+    console.log(formData);
+  }
+
   render() {
     const { post, getPost, goBack } = this.props;
 
     return (
       <div>
-        <form className="form-container">
+        <form className="form-container" onSubmit={(e) => this.onSubmit(e)}>
           <div className="field">
             <label className="form-label" htmlFor="post_title">Title</label>
             <input
@@ -17,6 +26,7 @@ class PostForm extends React.Component {
               type="text"
               name="post[title]"
               defaultValue={post.title}
+              ref={(input) => this.title = input}
             />
           </div>
 
@@ -26,10 +36,12 @@ class PostForm extends React.Component {
               id="post_body"
               className="form-input"
               name="post[body]"
-              defaultValue={post.body}></textarea>
+              defaultValue={post.body}
+              ref={(input) => this.body = input}></textarea>
           </div>
 
           <div className="field">
+            {/* Add ref for checkbox */}
             <label className="form-checkbox" htmlFor="post_published">
               <input name="post[published]" type="hidden" value="0"/>
               <input id="post_published" className="form-checkbox" type="checkbox" value="1" name="post[published]"/>
